79913a22a7c7c814c53028694bdc8f2b8d44ddcd
ivanarturosantivanezvela <iasantivanez@miuandes.cl>
2020-04-30 16:49:54 -0400

    song y video

--- ConsoleApp2/Artist.cs
+++ ConsoleApp2/Artist.cs
@@ -8,5 +8,43 @@ namespace ConsoleApp2
 {
     class Artist
     {
+        string username;
+        int ranking;
+        string biografia;
+        bool verification;
+        List<UserSystem> followers;
+
+        public Artist()
+        {
+            followers = new List<UserSystem>();
+            
+        
+        
+        }
+        public Artist(string Username,bool Verification, int Ranking ) 
+            :this()
+        
+        {
+            username = Username;
+            verification = Verification;
+            ranking = Ranking;
+        
+        
+        
+        
+        }
+
+
+
+
     }
+
+
+
+
+
+
+
+
+    
 }
